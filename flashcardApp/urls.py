4b1a4c95138787bbe7b0389dfa18f7c16e41d21d
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-20 19:34:51 +0300

    add the registration urls

---
+++ flashcardApp/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+
+urlpatterns=[
+    url(r'^$',views.index, name='index'),
+    url('register/',views.register, name='registration'),
+    url('login/',auth_views.LoginView.as_view(), name='login'),
+    url('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
