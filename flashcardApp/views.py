373753526ba9eec745d8ffedf73d84f509412a24
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-20 19:32:00 +0300

    define user registration view function in the views.py

--- flashcardApp/views.py
+++ flashcardApp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from flashcardApp.forms import SignUpForm
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+def register(request):
+    if request.method=="POST":
+        form=SignUpForm(request.POST) 
+        if form.is_valid():
+           form.save()
+           username = form.cleaned_data.get('username')
+           user_password = form.cleaned_data.get('password1')
+           user = authenticate(username=username, password=user_password)
+           login(request, user)
+        return redirect('login')
+    else:
+        form= SignUpForm()
+    return render(request, 'registration/registration_form.html', {"form":form})  
