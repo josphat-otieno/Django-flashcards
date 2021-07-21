69cb9a93f4cd8c9acbc5463dd4bd99e3e91368cd
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-21 07:21:18 +0300

    add config variables

--- flashcardProject/settings.py
+++ flashcardProject/settings.py
@@ -11,9 +11,46 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from pathlib import Path
+from decouple import config, Csv
+import django_on_heroku
+import dj_database_url
+
+
+MODE=config("MODE",default="dev")
+SECRET_KEY=config('SECRET_KEY')
+DEBUG=config('DEBUG',default=False, cast=bool)
+
+if config('MODE')=="dev":
+    DATABASES={
+        'default':{
+            'ENGINE':  'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': '',
+        }
+    }
+
+#production
+else:
+    DATABASES={
+        'default': dj_database_url.config(
+            default=config('DATABASE_URL')
+        )
+    }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
