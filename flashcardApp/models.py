45be55f2a2cde1cb6df6b085b34e8932c90efb83
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-21 07:30:53 +0300

    add model flashcard

--- flashcardApp/models.py
+++ flashcardApp/models.py
@@ -4,4 +4,15 @@ from django.db import models
 
 class Subject(models.Model):
     subject_name = models.CharField(max_length=60)
-    
+
+    def __str__(self):
+        return self.subject_name
+
+class Flashcard(models.Model):
+    flash_image = models.ImageField(upload_to='images/')
+    flash_title = models.CharField(max_length=60 )
+    flash_notes = models.TextField()
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.flash_title
