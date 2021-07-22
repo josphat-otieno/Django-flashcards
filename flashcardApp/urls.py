7999353e024e17ae705158e674eff06b092044fc
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-22 15:44:41 +0300

    add urls for getting single objects

--- flashcardApp/urls.py
+++ flashcardApp/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     url(r'^$',views.index, name='index'),
     path('api/flashcard/', FlashcardView.as_view(), name='FlashcardView'),
     path('api/flashcards/(?P<pk>[0-9]+)/$',FlashMember.as_view()),
+    path('api/subjects/<int:subject_id>/', views.flash_detail_view, name='flash_detail_view'),
+    path('api/subjects/<int:subject_id>/flashcards/', views.flashcard_view, name='flashcard_view')
     # path('api/flashcard/<int:pk>', FlashcardView.as_view()),
     # path('api/flashcard/<int:pk>', SingleFlashcardView.as_view()),
     # url(r'^api/v1/posts/$', 'post_collection'),
